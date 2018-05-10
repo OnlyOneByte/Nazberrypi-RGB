e379f3df11c03f29f2f13694ae82f5d8f8043c2d
Angelo Yang <angelo.r.yang@gmail.com>
2018-05-09 20:53:30 -0500

    small change for my rekt raspi zero w

--- rgb.py
+++ rgb.py
@@ -10,7 +10,8 @@ blue = sys.argv[3]
 pi = pigpio.pi()
 
 #old 17,24,22
+#imma be using 13, 19, 26
 #replace first argument below with the GPIO Pin number associated with the specified color
-pi.set_PWM_dutycycle(18,red)
-pi.set_PWM_dutycycle(24,green)
-pi.set_PWM_dutycycle(20,blue)
+pi.set_PWM_dutycycle(13,red)
+pi.set_PWM_dutycycle(19,green)
+pi.set_PWM_dutycycle(26,blue)
